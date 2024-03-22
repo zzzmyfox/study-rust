f636c6348c905da1500e7dbb096326085fe426f4
wangzhe <wangzhe@fox-ess.com>
2024-03-22 14:48:54 +0800

    study: thread

---
+++ thread/src/main.rs
@@ -0,0 +1,15 @@
+use std::thread::{self, sleep};
+use std::time::Duration;
+
+fn main() {
+    let v = vec![1,2,3,4];
+
+    let handle = thread::spawn(move || {
+        println!("here's a vector: {:?}", v);
+    });
+
+    handle.join().unwrap();
+
+
+    thread::sleep(Duration::from_secs(10));
+}
